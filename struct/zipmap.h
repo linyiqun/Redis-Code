69bdab968a7206ef9bef1e8e2910b2a1507d7fa0
linyiqun <675250079@qq.com>
2014-10-12 19:04:58 +0800

    zipmap.h头文件的主要信息
    描述了一些zipmap中主要的一些方法

--- struct/zipmap.h
+++ struct/zipmap.h
@@ -35,15 +35,15 @@
 #ifndef _ZIPMAP_H
 #define _ZIPMAP_H
 
-unsigned char *zipmapNew(void);
-unsigned char *zipmapSet(unsigned char *zm, unsigned char *key, unsigned int klen, unsigned char *val, unsigned int vlen, int *update);
-unsigned char *zipmapDel(unsigned char *zm, unsigned char *key, unsigned int klen, int *deleted);
-unsigned char *zipmapRewind(unsigned char *zm);
-unsigned char *zipmapNext(unsigned char *zm, unsigned char **key, unsigned int *klen, unsigned char **value, unsigned int *vlen);
-int zipmapGet(unsigned char *zm, unsigned char *key, unsigned int klen, unsigned char **value, unsigned int *vlen);
-int zipmapExists(unsigned char *zm, unsigned char *key, unsigned int klen);
-unsigned int zipmapLen(unsigned char *zm);
-size_t zipmapBlobLen(unsigned char *zm);
-void zipmapRepr(unsigned char *p);
+unsigned char *zipmapNew(void);  //创建一个新的压缩图
+unsigned char *zipmapSet(unsigned char *zm, unsigned char *key, unsigned int klen, unsigned char *val, unsigned int vlen, int *update); //设置压缩图中的某个键值对
+unsigned char *zipmapDel(unsigned char *zm, unsigned char *key, unsigned int klen, int *deleted);  //删除压缩图上的某个键值对
+unsigned char *zipmapRewind(unsigned char *zm);   //将在zipmapNext中被调用到
+unsigned char *zipmapNext(unsigned char *zm, unsigned char **key, unsigned int *klen, unsigned char **value, unsigned int *vlen); //取得此键值对的下一个键值对
+int zipmapGet(unsigned char *zm, unsigned char *key, unsigned int klen, unsigned char **value, unsigned int *vlen); //获取某个键值对
+int zipmapExists(unsigned char *zm, unsigned char *key, unsigned int klen); //某个key值在zipmap中是否存在
+unsigned int zipmapLen(unsigned char *zm); //zipmap压缩图的总键值对数
+size_t zipmapBlobLen(unsigned char *zm); //压缩图的序列化到文件中所需大小
+void zipmapRepr(unsigned char *p);  //输出的压缩图的具体信息，用于测试
 
 #endif
